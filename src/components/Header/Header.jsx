ae3165a2ec4b9c2f57fba15dd548688f435d53b7
mpreter <dshabelyan@mail.ru>
2023-05-08 15:06:08 +0300

    added nav-burger to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,55 +1,84 @@
-import NavBurger from "../NavBurger/NavBurger";
+import { useState, useRef, useEffect } from "react";
+import classNames from "classnames";
+import { Link } from 'react-router-dom';
 
+import { navigationItems } from "./constants";
 import "./Header.scss";
 
 const Header = () => {
+  const navigationRef = useRef(null);
+  const [openBurger, setOpenBurger] = useState(false);
+
+  const handleBurgerClick = () => {
+    setOpenBurger(!openBurger);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        navigationRef.current &&
+        !navigationRef.current.contains(event.target)
+      ) {
+        setOpenBurger(false);
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [navigationRef]);
+
   return (
     <header className="header">
       <div className="header__container">
-        <a href="/" className="header__logo-container">
+        <Link href="/" className="header__logo-container">
           <div className="header__logo"></div>
           <div className="header__logo-corner header__logo-corner_top-left"></div>
           <div className="header__logo-corner header__logo-corner_top-right"></div>
           <div className="header__logo-corner header__logo-corner_bottom-right"></div>
           <div className="header__logo-corner header__logo-corner_bottom-left"></div>
-        </a>
-        <div className="nav-menu">
-          <ul className="nav-menu__container">
-            <li className="nav-menu__item">
-              <a href="/" className="nav-menu__link">
-                Главная
-              </a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="/mock-address/change-me" className="nav-menu__link">
-                Технология
-              </a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="/mock-address/change-me" className="nav-menu__link">
-                График полетов
-              </a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="/mock-address/change-me" className="nav-menu__link">
-                Гарантии
-              </a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="/mock-address/change-me" className="nav-menu__link">
-                О компании
-              </a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="/mock-address/change-me" className="nav-menu__link">
-                Контакты
-              </a>
-            </li>
+        </Link>
+        <div className="header__nav-menu">
+          <ul className="header__nav-menu-container">
+            {navigationItems.map((item) => (
+              <li className="header__nav-menu-item" key={item.text}>
+                <Link href={item.to} className="header__nav-menu-link">
+                  {item.text}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
-        <NavBurger />
+        <div className="header__nav-burger" ref={navigationRef}>
+          <button
+            className="header__nav-burger-button-menu"
+            type="button"
+            onClick={handleBurgerClick}
+          >
+            <div
+              className={classNames("header__nav-burger-button-menu-lines", {
+                "header__nav-burger-button-menu-lines_active": openBurger,
+              })}
+            ></div>
+          </button>
+          <ul
+            className={classNames("header__nav-burger-menu-burger", {
+              "header__nav-burger-menu-burger_open": openBurger,
+            })}
+          >
+            {navigationItems.map((item) => (
+              <li className="header__nav-burger-item" key={item.text}>
+                <Link href={item.to} className="header__nav-burger-link">
+                  {item.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="header__background"></div>
+      <div className="header__background" />
     </header>
   );
 };
---
+++ src/components/Header/constants.js
@@ -0,0 +1,12 @@
+import { SCREENS } from '../../routes/endpoints';
+
+const navigationItems = [
+  { text: 'Главная', to: SCREENS.LANDING },
+  { text: 'Технология', to: '/mock-address/change-me' },
+  { text: 'График полетов', to: '/mock-address/change-me' },
+  { text: 'Гарантии', to: '/mock-address/change-me' },
+  { text: 'О компании', to: '/mock-address/change-me' },
+  { text: 'Контакты', to: '/mock-address/change-me' },
+];
+
+export { navigationItems };
